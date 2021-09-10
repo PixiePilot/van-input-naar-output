010fdbc57f8a81e2e6d2e0d3fbde563df6ce1e9c
PixiePilot <89778100+PixiePilot@users.noreply.github.com>
2021-09-10 12:03:16 +0200

    Add files via upload

---
+++ feestlunch2.py
@@ -0,0 +1,3 @@
+bedrag = float(input('Bedrag: '))
+bedrag = str(bedrag)
+print('De feestlunch kost je bij de bakker ' + bedrag + ' euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
